7cdf198b924e1c9054477f5d9dcc0dd906c5c25f
deluscc4 <delusccafs@icloud.com>
2024-02-11 16:00:43 -0300

    my studies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class Dadoos extends StatelessWidget {
+class Dadoos extends StatefulWidget {
+  @override
+  State<Dadoos> createState() => _DadoosState();
+}
+
+class _DadoosState extends State<Dadoos> {
+  int esquerdo = 1;
+  int direito = 1;
+
+  void girarDados() {
+    setState(() {
+      esquerdo = Random().nextInt(6) + 1;
+      direito = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                  girarDados();
+              },
+              child: Image.asset('imagens/dado$esquerdo.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                  girarDados();
+              },
+              child: Image.asset('imagens/dado$direito.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
